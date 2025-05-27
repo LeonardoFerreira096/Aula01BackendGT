32cb77dfb0bdcae161eb3bee8c7b175736e2f416
Leonardo Costa Ferreira <leonardo_costaferreira22@hotmail.com>
2025-05-27 15:02:36 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs'); 
+const os = require('os');
+const chalk = require('chalk');
+
+console.log('Sistema Operacional:', os.platform());
+console.log('Arquitetura:', os.arch());
+console.log('Usuário:', os.userInfo());
+
+console.log(chalk.blue('Texto azul'));
